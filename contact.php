669edafe5fd6a36399bf785f1431f50397252c3f
Abhishek <72224235+Abhishek-Jajoria@users.noreply.github.com>
2020-11-04 12:27:21 +0530

    Update and rename Process.php to contact.php

--- contact.php
+++ contact.php
@@ -14,7 +14,7 @@ else
     mail("ajazzy.an@gmail.com", "AJ Message" , $message,"From: $name <$email>" );
     echo "<script type='text/javascript'>alert('Your message sent successfully');
         window.history.log(-1);
-    </script>"
+    </script>";
 
 
 }
