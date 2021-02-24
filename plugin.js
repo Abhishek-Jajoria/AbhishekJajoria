6f5ddebb73778e1e74726349729809f2b3eeb04b
Abhishek <72224235+Abhishek-Jajoria@users.noreply.github.com>
2021-02-24 15:28:33 +0530

    add dark mode

--- plugin.js
+++ plugin.js
@@ -17,3 +17,26 @@
      document.body.scrollTop = 0; // For Safari
      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
  };
+
+//  -------------- DARK MODE --------------------
+
+// new Darkmode().showWidget();
+
+const options = {
+    
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.3s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+    
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
